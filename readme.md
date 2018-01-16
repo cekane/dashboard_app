d83f3880d17eecd1fe9d38fc4ea04abc9243e3a1
cekane <ckane144@gmail.com>
2018-01-15 22:06:33 -0500

    Now able to add to database and get from database

--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # Header
 ## Starting the application
 1. `npm run dev` Starts the server with nodemon
+2. `C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe` Starts the local mongodb server
 
 ## Resources for learning
 1. [Initial Setup](https://zellwk.com/blog/crud-express-mongodb/) 
--- server.js
+++ server.js
@@ -17,9 +17,7 @@ MongoClient.connect(localUrl, (err, database) => {
     } else{
         console.log("Successfully connected to mongodb server ...")
     }
-    console.log("before", db)
     db = database.db(localDatabaseName)
-    console.log("after", db)
     app.listen(port , () => {
         console.log('listening on '+ port)
     })
@@ -29,11 +27,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     //__dirname is directory that contains the JavaScript source code
+    db.collection('TestCollection').find().toArray((err,results)=>{
+        console.log(results)
+    })
     res.sendFile(__dirname + '/index.html')
 });
 
 app.post('/quotes', (req, res) => {
-    console.log("in quotes thing", db)
     db.collection('TestCollection').save(req.body, (err, result) => {
         if(err) {return console.log(err)}
         else{ console.log("Successfully inserted into TestCollection")}
